0778bfb6672fbaec0ea74450258c80a2e89cf9dd
Kirst8 <kirst8.brooker@gmail.com>
2021-08-02 18:20:55 +0000

    updated requirements.txt

--- checkout/views.py
+++ checkout/views.py
@@ -14,7 +14,7 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_0SMREd7Vdweb1MGRi8S0EycR00JVzSAs5O',
+        'stripe_public_key': 'pk_test_51JJrFdDLJJ7bNy0mAFKBk9kkHCpVYUaCIZBHUB7D8TgH8Z5JIUvRdQxtlvwch4TGrLH85sieGKKGJppX6g3WOmoN00mc4wWE0s'
         'client_secret': 'test client secret',
     }
 
